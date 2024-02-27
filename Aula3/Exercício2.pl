99509dfd7484683f47799c8405a9aa362b6f1802
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-02-27 14:42:36 -0300

    Create Exercício2.pl

---
+++ Aula3/Exercício2.pl
@@ -0,0 +1,40 @@
+%BASE DE CONHECIMENTO
+%matéria que estuda
+estuda(fabricio, programação).
+estuda(joana, lógica).
+estuda(ana, tcc). 
+estuda(josé, banco).
+estuda(lucas, estrutura).
+estuda(nicolas, lógica).
+estuda(fernanda, tcc).
+estuda(maria, tcc).
+
+%aluno e faculdade
+aluno(fabricio, ufmg).
+aluno(joana, ufu).
+aluno(ana, ufu).
+aluno(josé, ufu).
+aluno(lucas, ufmg).
+aluno(nicolas, ufu).
+aluno(fernanda, ufu).
+aluno(maria, ufu).
+
+%professor e matéria
+professor(joão, programação).
+professor(clara, lógica).
+professor(roberta, tcc).
+professor(antonio, banco).
+professor(leandro, estrutura).
+
+%reitor e faculdade
+reitor(beatriz, ufmg).
+reitor(marcos, ufu).
+
+%curso e matéria que oferta
+oferta(bcc, programação).
+oferta(bcc, lógica).
+oferta(si, tcc).
+oferta(si, banco).
+oferta(bcc, estrutura).
+
+%REGRAS 
