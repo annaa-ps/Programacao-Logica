3b7d326af10aef92aa0b7833b8ffc54b88f881d5
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-02-27 16:14:16 -0300

    Update Exercício2.pl

--- Aula3/Exercício2.pl
+++ Aula3/Exercício2.pl
@@ -33,6 +33,28 @@ reitor(marcos, ufu).
 %curso e matéria que oferta
 oferta(bcc, programação).
 oferta(bcc, lógica).
+
+%REGRAS 
+%Regra 1
+alunos_professor(X,Y):-estuda(X,Z), professor(Y,Z). 
+
+%Regra 2
+alunos_bcc(X):-estuda(X,Y), oferta(bcc,Y). 
+
+%Regra 3
+alunos_si(X):-estuda(X,Y), oferta(si, Y). 
+
+%Regra 4 
+alunos_universidade(X,Y):-aluno(X,Y). 
+
+%Regra 5 
+disciplinas_professor(X,Y):-professor(Y,X). 
+    
+%Regra 6 
+professor_universidade(X,Y):-professor(X,Z), estuda(A,Z), aluno(A,Y). 
+
+%Regra 7 
+disciplinas_universidade(X,Y):-estuda(A,X), aluno(A,Y).
 oferta(si, tcc).
 oferta(si, banco).
 oferta(bcc, estrutura).
