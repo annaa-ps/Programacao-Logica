bb5b22632713bf65729e0154a707b0d03eab55d1
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-02-28 15:36:14 -0300

    Create Exercício2.pl

---
+++ Aula4/Exercício2.pl
@@ -0,0 +1,15 @@
+%Banco De Dados    
+dados:- 
+    	  write('Informe seu nome:'),
+    	  read(Nome),nl,
+    	  write('Digite sua nota da 1º Disciplina:'), 
+          read(Nota1),nl,
+    	  write('Digite sua nota da 2º Disciplina:'), 
+          read(Nota2),nl,
+    	  write('Digite sua nota da 3º Disciplina:'), 
+          read(Nota3),nl,
+    	  write('Digite sua nota da 4º Disciplina:'), 
+          read(Nota4),nl,
+    	  write('Digite sua nota da 5º Disciplina:'), 
+          read(Nota5),nl.
+    
