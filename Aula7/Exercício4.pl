14d0c36076577e337c676a65d3126c1f3d97a74a
Anna Karolyna <129415573+annaa-ps@users.noreply.github.com>
2024-04-07 15:02:14 -0300

    Create Exercício4.pl

---
+++ Aula7/Exercício4.pl
@@ -0,0 +1,14 @@
+%4) Calcular a soma dos primeiros n-valores inteiros:
+%             S(n) = 1 + 2 + 3 + 4 + ..... + (n − 1) + n
+%             
+% S(n) = 1       para n = 1
+% S(n-1) + n     para n >= 2
+
+%Caso Base: Se n = 1 a soma vai ser = 1 
+soma_valores(1,1). 
+
+%Caso recursivo: 
+soma_valores(N, S):- N > 1, 
+    				 N1 is N - 1, 
+    				 soma_valores(N1,Soma), 
+    				 S is Soma + N.  
